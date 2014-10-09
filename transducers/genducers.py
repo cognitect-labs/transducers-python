0e9edeab6ee421c6a24592d2220f64db924748c6
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-10-08 21:41:13 -0600

    A menagerie of pseudotransducers.

--- transducers/genducers.py
+++ transducers/genducers.py
@@ -32,6 +32,76 @@ def filtering(f):
     return partial(filter, f)
 
 
+def remove(pred):
+    """Remove genducer."""
+    def generator(coll):
+        for item in coll:
+            if not pred(item):
+                yield item
+    return generator
+
+
+def take_while(pred):
+    """Keep taking until pred is false."""
+    def generator(coll):
+        for item in coll:
+            if pred(item):
+                yield item
+            else:
+                raise StopIteration
+    return generator
+
+def drop(n):
+    """Drop until n"""
+    def generator(coll):
+        for i, item in enumerate(coll):
+            if i >= n:
+                yield item
+    return generator
+
+def drop_while(pred):
+    def generator(coll):
+        trigger = False
+        for item in coll:
+            if trigger:
+                yield item
+            elif not pred(item):
+                trigger = True
+                yield item
+    return generator
+
+def take_nth(n):
+    def generator(coll):
+        for i, item in enumerate(coll):
+            if not i % n:
+                yield item
+    return generator
+
+def replace(smap):
+    def generator(coll):
+        for item in coll:
+            if item in smap:
+                yield smap[item]
+            else:
+                yield item
+    return generator
+
+def keep(pred):
+    def generator(coll):
+        for item in coll:
+            res = pred(item)
+            if res is not None:
+                yield res
+    return generator
+
+def keep_indexed(f):
+    def generator(coll):
+        for idx, item in enumerate(coll):
+            res = f(idx, item)
+            if res is not None:
+                yield res
+    return generator
+
 def dedupe(coll):
     prev = next(coll)
     yield prev
