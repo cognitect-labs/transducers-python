05552338c70ef5792bc5235aaf73c9380315bde7
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-10-11 08:07:37 -0600

    Show where Cython backend can be turned on (dev only at the moment).

--- transducers/__init__.py
+++ transducers/__init__.py
@@ -1,2 +1,6 @@
+# Uncomment this line and comment line below to test Cython.
+# -- Cython only compatible with version of Python built against,
+# -- not compatible with PyPy.
+# from . import cgenducers as genducers
 from . import genducers
 from .genducers import *
