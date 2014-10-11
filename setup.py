5f7e3cbbfda2508443455aef39e839f4fdc86dac
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-10-11 09:25:23 -0600

    Building artifacts for package/release.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
+#from Cython.Build import cythonize
 
 # This isn't a real setup file yet, just the hooks for Cythonize.
-setup(ext_modules = cythonize("transducers/cgenducers.pyx"))
+setup(name="transducers",
+#      version="0.4" + revision, <-- need to hook up
+      description="Transducer semantics for Python generators.",
+      author="Cognitect",
+      url="https://github.com/cognitect-labs/transducers-python",
+      packages=find_packages())
+ #     ext_modules = cythonize("transducers/cgenducers.pyx"))
