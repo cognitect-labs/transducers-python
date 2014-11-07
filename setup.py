edc48bf5c58d605db67f55a57277d8d6ec9fae76
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-11-07 09:47:47 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ from setuptools import setup, find_packages
 # This isn't a real setup file yet, just the hooks for Cythonize.
 setup(name="transducers",
 #      version="0.4" + revision, <-- need to hook up
-      description="Transducer semantics for Python generators.",
+      description="Clojure Transducers implemented in Python",
       author="Cognitect",
       url="https://github.com/cognitect-labs/transducers-python",
       packages=find_packages())
- #     ext_modules = cythonize("transducers/cgenducers.pyx"))
